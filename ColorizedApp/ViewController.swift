6125187ffa4451326bec1e64ab0b81905c290a48
iOSipovDobrynia <dobrynia.osipov@gmail.com>
2023-05-01 14:46:17 +0300

    add logic

--- ColorizedApp/ViewController.swift
+++ ColorizedApp/ViewController.swift
@@ -9,11 +9,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: IBOutlets
+    @IBOutlet var rgbView: UIView!
+    @IBOutlet var redValueLabel: UILabel!
+    @IBOutlet var greenValueLabel: UILabel!
+    @IBOutlet var blueValueLabel: UILabel!
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        rgbView.layer.cornerRadius = 10
     }
 
-
+    // MARK: IBActions
+    @IBAction func redSliderAction() {
+        redValueLabel.text = round(redSlider.value).formatted()
+        changeColor(of: rgbView)
+    }
+    
+    @IBAction func greenSliderAction() {
+        greenValueLabel.text = round(greenSlider.value).formatted()
+        changeColor(of: rgbView)
+    }
+    
+    @IBAction func blueSliderAction() {
+        blueValueLabel.text = round(blueSlider.value).formatted()
+        changeColor(of: rgbView)
+    }
+    
+    // MARK: privat methods
+    private func changeColor(of view: UIView) {
+        view.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value) / 255,
+            green: CGFloat(greenSlider.value) / 255,
+            blue: CGFloat(blueSlider.value) / 255,
+            alpha: 1
+        )
+    }
 }
 
